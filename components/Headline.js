56cbc6ac838f3e859ce990e37fc03e158012baaa
fumn112026 <fumn112026@gmail.com>
2024-07-21 14:30:21 +0900

    🎨 Headlineをコンポーネント化

---
+++ components/Headline.js
@@ -0,0 +1,30 @@
+import styles from "@/styles/Home.module.css";
+import Image from "next/image";
+
+export default function Headline(props) {
+  return (
+    <div className={styles.description}>
+      <p>
+        Hello!! Get started by editing&nbsp;
+        <code className={styles.code}>{props.page}.js</code>
+      </p>
+      <div>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          By{" "}
+          <Image
+            src="/vercel.svg"
+            alt="Vercel Logo"
+            className={styles.vercelLogo}
+            width={100}
+            height={24}
+            priority
+          />
+        </a>
+      </div>
+    </div>
+  );
+}
