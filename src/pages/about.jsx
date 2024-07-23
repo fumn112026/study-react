81fe9d83b702e3b0325e90d2a7f6613ba440e8d4
fumn112026 <fumn112026@gmail.com>
2024-07-23 09:47:06 +0900

    Custom Hooksに切り出し

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,17 +4,36 @@ import { Links } from "src/components/Links";
 import { Headline } from "src/components/Headline";
 import { Header } from "src/components/Header";
 import { Logo } from "src/components/Logo";
+import { useCounter } from "/src/hooks/useCounter";
+import { useInputArray } from "/src/hooks/useInputArray";
+import { useBgLightBlue } from "@/src/hooks/useBgLightBlue";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function About() {
+  const { count, isShow, handleCount, handleDisplay } = useCounter();
+  const { text, array, handleChange, handleAdd } = useInputArray();
+  useBgLightBlue();
+
   return (
     <>
       <Header page="About" />
       <main className={`${styles.main} ${inter.className}`}>
-        <Headline page="about page" />
+        <Headline page="about" />
         <Logo />
         <Links />
+
+        <button onClick={handleCount}>ボタン</button>
+        {isShow ? <h1>{count}</h1> : null}
+        <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+
+        <input type="text" value={text} onChange={handleChange} />
+        <button onClick={handleAdd}>追加</button>
+        <ul>
+          {array.map((item) => {
+            return <li key={item}>{item}</li>;
+          })}
+        </ul>
       </main>
     </>
   );
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,30 +4,16 @@ import { Links } from "src/components/Links";
 import { Headline } from "src/components/Headline";
 import { Header } from "src/components/Header";
 import { Logo } from "src/components/Logo";
-import { useCallback, useState } from "react";
+import { useCounter } from "/src/hooks/useCounter";
+import { useInputArray } from "/src/hooks/useInputArray";
+import { useBgLightBlue } from "@/src/hooks/useBgLightBlue";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [count, setCount] = useState(1);
-  const [text, setText] = useState("");
-  const [isShow, setIsShow] = useState(true);
-
-  const handleCount = useCallback((e) => {
-    setCount((count) => count + 1);
-  }, []);
-
-  const handleChange = useCallback((e) => {
-    if (e.target.value.length > 5) {
-      alert("5文字以下にしてください。");
-      return;
-    }
-    setText(e.target.value.trim());
-  }, []);
-
-  const handleDisplay = useCallback(() => {
-    setIsShow((isShow) => !isShow);
-  }, []);
+  const { count, isShow, handleCount, handleDisplay } = useCounter();
+  const { text, array, handleChange, handleAdd } = useInputArray();
+  useBgLightBlue();
 
   return (
     <>
@@ -36,11 +22,19 @@ export default function Home() {
         <Headline page="index" />
         <Logo />
         <Links />
+
         <button onClick={handleCount}>ボタン</button>
         {isShow ? <h1>{count}</h1> : null}
         <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+
         <input type="text" value={text} onChange={handleChange} />
+        <button onClick={handleAdd}>追加</button>
+        <ul>
+          {array.map((item) => {
+            return <li key={item}>{item}</li>;
+          })}
+        </ul>
       </main>
     </>
-  )
+  );
 }
